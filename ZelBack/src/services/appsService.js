21b150717db76ebbcf01f413959e2cf1c2e45c39
David White <david.white@trustpower.co.nz>
2024-03-04 15:43:43 +0000

    Add debug

--- ZelBack/src/services/appsService.js
+++ ZelBack/src/services/appsService.js
@@ -1619,7 +1619,7 @@ async function fluxUsage(req, res) {
  * @returns {object} Message.
  */
 async function appsResources(req, res) {
-  log.info('Checking appsResources');
+  log.info('Checking apps resources');
   try {
     const dbopen = dbHelper.databaseConnection();
     const appsDatabase = dbopen.db(config.database.appslocal.database);
@@ -1674,6 +1674,8 @@ async function appsResources(req, res) {
       error.code,
     );
     return res ? res.json(errorResponse) : errorResponse;
+  } finally {
+    log.info("Finished checking apps resources");
   }
 }
 
@@ -10104,6 +10106,7 @@ async function stopAllNonFluxRunningApps() {
  */
 async function forceAppsRemoval(appNames) {
   log.info(`FORCING REMOVAL: ${appNames}`);
+  log.info(appNames);
   // eslint-disable-next-line no-restricted-syntax
   for (const appName of appNames) {
     // eslint-disable-next-line no-await-in-loop
